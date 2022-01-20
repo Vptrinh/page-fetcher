f9f54351411bc8ff04bb1b2525a77e5367c1fed5
Vivian Trinh <vptrinh@ryerson.ca>
2022-01-19 19:11:45 -0500

    Created a command line program to download the number of bytes from a website to a file path.

---
+++ fetcher.js
@@ -0,0 +1,20 @@
+//take two command line arguments 1: URL 2: a local file path
+const request = require('request');
+const fs = require('fs');
+
+if (process.argv.length === 4) {
+  let website = process.argv.slice(2)[0];
+  let filePath = process.argv.slice(2)[1];
+
+  request(website, (error, response, body) => {
+    if (error) {
+      throw error;
+    }
+    fs.writeFile(filePath, body, (error) => {
+      if (error) {
+        throw error;
+      }
+      console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`);
+    });
+  });
+}
